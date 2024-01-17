a4d2578022f4cab299ca4f472adc95863f19dac0
franms333 <franms099@gmail.com>
2024-01-17 10:15:33 -0300

    test deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/franms333.github.io'
+  base: '/franms333.github.io/',
+  plugins: [react()]
 })
